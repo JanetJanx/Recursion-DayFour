1fd067c295268e029c3204aeca5d2d709036b503
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-05 16:24:26 +0300

    sum of list items using recursion

--- sum_item_list.py
+++ sum_item_list.py
@@ -1,21 +1,18 @@
 #function to sum up items in a list
-def sum_items(a):
+def sum_items(list_number):
     sum = 0
-    sum_inside_list = 0
-    for x in a:
-        if type(x) == int:
-            sum +=x
-        
-        elif isinstance(x, list):
-            for i in x:
-                if type(i) == int:
-                    sum_inside_list += i
+    for item in list_number:
+        if type(item) == int:
+            sum += item
+        elif isinstance(item, list):
+                if type(sum_items(item)) == int:
+                    sum += sum_items(item)
                 else:
                     return "Inside List should only have numbers"
         else:
             return "List should only have numbers"
     
-    return sum + sum_inside_list
+    return sum 
 
 print (sum_items([1,5,9,[3,'n']]))
 print (sum_items([3,7,8,[8,9]]))
